90e3f4b2bedafc6d1ee53ce135c81ed8cd24686e
Henry-NetAss <142622280+Henry-NetAss@users.noreply.github.com>
2025-03-31 13:18:45 +0200

    Winget_Check_Update.ps1

---
+++ Winget_Check_Update.ps1
@@ -0,0 +1,9 @@
+$progressPreference = 'silentlyContinue'
+Write-Host "Installing WinGet PowerShell module from PSGallery..."
+Install-PackageProvider -Name NuGet -Force | Out-Null
+Install-Module -Name Microsoft.WinGet.Client -Force -Repository PSGallery | Out-Null
+Write-Host "Using Repair-WinGetPackageManager cmdlet to bootstrap WinGet..."
+Repair-WinGetPackageManager
+Write-Host "Done."
+echo y|winget upgrade --all --silent
+exit
